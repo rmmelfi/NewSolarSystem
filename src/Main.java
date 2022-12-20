ffed937fd1627bc4c036c3947f30280c0b01e2a5
rmmelfi <rmmelfi@gmail.com>
2022-12-20 01:19:02 -0500

    The basics of Solar System are ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem Rachael = new SolarSystem();
+        Rachael.moon=1;
+        Rachael.sun=2;
+        Rachael.Planet=3;
+
+    }
+}
